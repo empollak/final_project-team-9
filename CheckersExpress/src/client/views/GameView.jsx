55cd2eed5cf2f254fe8f621dd5473279d048bbb8
krutgoat <krutics@outlook.com>
2024-10-07 12:33:36 -0400

    attempting to draw a simple board

--- CheckersExpress/src/client/views/GameView.jsx
+++ CheckersExpress/src/client/views/GameView.jsx
@@ -3,9 +3,19 @@
 export default function redrawBoard(board) {
     const ctx = canvasObj.getContext('2d')
     
-    for (let i = 0; i <= 8; i++) {
+    // clear the canvas area before rendering elements again
+    ctx.clearRect(0, 0, canvasObj.width, canvasObj.height) // assume square region
 
-        
+    // draw game board
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            ctx.fillStyle = 'white'
+            ctx.fillRect(sq.column * 60, sq.row * 60, 60, 60) // filling the squares
+            ctx.fillStyle = 'black'
+            ctx.strokeRect(sq.column * 60, sq.row * 60, 60, 60) // adding borders to the squares
+        }
     }
+
+    // draw tokens
 
 }
