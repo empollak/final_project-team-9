b14c3b712cb9c442b6ac29c63fb74ef961217d1d
krutgoat <krutics@outlook.com>
2024-10-09 18:46:59 -0400

    password doesn't show up in plaintext

--- CheckersExpress/src/client/routes/login.jsx
+++ CheckersExpress/src/client/routes/login.jsx
@@ -37,33 +37,12 @@ export function LoginPage() {
     async function login(request) {
         request.preventDefault();
         console.log("submitted", request);
-        // let formData = await request.formData();
-
-        // // Intent is based on the button value
-        // // Differentiates between login and register
-        // let intent = formData.get("intent");
-        // console.log("Sending ", formData.get("username"), "pw", formData.get("password"));
-        // let resource = (intent === "login") ? "/login" : "/register";
-
-        // // Either login or register depending on request. Both are handled the same way. 
-        // const response = await fetch(resource, {
-        //     method: "POST",
-        //     headers: { "Content-Type": "application/json" },
-        //     body: JSON.stringify({ username: formData.get("username"), password: formData.get("password") }),
-        // })
-        // if (response.status === 201) {
-        //     console.log("good");
-        //     return redirect("/browser");
-        // } else {
-        //     // Login failed for some reason. Return the text error message to be displayed.
-        //     return await response.text();
-        // }
     }
 
     return (<>
         <Form id="login-form" method="post">
-            <input placeholder="username" name="username" />
-            <input placeholder="password" name="password" />
+            <input type="text" placeholder="username" name="username" />
+            <input type="password" placeholder="password" name="password" />
             <button type="submit" name="intent" value="register">Register</button>
             <button type="submit" name="intent" value="login">Log in</button>
             <p>{useActionData()}</p>
